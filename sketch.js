861e9da39332f302fa6ad459274dee674e0ea128
Garrett Neiss <garrett@neiss.io>
2017-04-22 11:52:15 -0400

    Change column 5 to blue

--- sketch.js
+++ sketch.js
@@ -119,25 +119,25 @@ var n = 3;
 
 var n = 4;
   //Column 5
-  fill(color(a));
+  fill(color(e));
   rect(blockwidth * n, 0, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(e));
   rect(blockwidth * n, blockheight, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(e));
   rect(blockwidth * n, blockheight * 2, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(e));
   rect(blockwidth * n, blockheight * 3, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(e));
   rect(blockwidth * n, blockheight * 4, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(e));
   rect(blockwidth * n, blockheight * 5, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(e));
   rect(blockwidth * n, blockheight * 6, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(e));
   rect(blockwidth * n, blockheight * 7, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(e));
   rect(blockwidth * n, blockheight * 8, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(e));
   rect(blockwidth * n, blockheight * 9, blockwidth, blockheight)
 
 var n = 5;
