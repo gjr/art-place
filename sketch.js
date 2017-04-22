f82487ac1ff1148c8d580a352bd6f4f2f0d13082
Garrett Neiss <garrett@neiss.io>
2017-04-22 11:54:01 -0400

    Change column 7 to pink

--- sketch.js
+++ sketch.js
@@ -165,25 +165,25 @@ var n = 5;
 
 var n = 6;
   //Column 7
-  fill(color(a));
+  fill(color(g));
   rect(blockwidth * n, 0, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(g));
   rect(blockwidth * n, blockheight, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(g));
   rect(blockwidth * n, blockheight * 2, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(g));
   rect(blockwidth * n, blockheight * 3, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(g));
   rect(blockwidth * n, blockheight * 4, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(g));
   rect(blockwidth * n, blockheight * 5, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(g));
   rect(blockwidth * n, blockheight * 6, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(g));
   rect(blockwidth * n, blockheight * 7, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(g));
   rect(blockwidth * n, blockheight * 8, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(g));
   rect(blockwidth * n, blockheight * 9, blockwidth, blockheight)
 
 var n = 7;
