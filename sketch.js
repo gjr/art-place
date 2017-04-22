d616bf55ab3e3c9b76726aff50ce2ed7968e1443
Garrett Neiss <garrett@neiss.io>
2017-04-22 11:50:37 -0400

    Change column 3 to yellow

--- sketch.js
+++ sketch.js
@@ -57,30 +57,40 @@ function draw() {
   rect(blockwidth, blockheight * 7, blockwidth, blockheight)
   fill(color(b));
   rect(blockwidth, blockheight * 8, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(b));
   rect(blockwidth, blockheight * 9, blockwidth, blockheight)
 
 var n = 2;
   //Column 3
-  fill(color(a));
+  fill(color(c));
+  fill(color(c));
   rect(blockwidth * n, 0, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(c));
+  fill(color(c));
   rect(blockwidth * n, blockheight, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(c));
+  fill(color(c));
   rect(blockwidth * n, blockheight * 2, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(c));
+  fill(color(c));
   rect(blockwidth * n, blockheight * 3, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(c));
+  fill(color(c));
   rect(blockwidth * n, blockheight * 4, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(c));
+  fill(color(c));
   rect(blockwidth * n, blockheight * 5, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(c));
+  fill(color(c));
   rect(blockwidth * n, blockheight * 6, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(c));
+  fill(color(c));
   rect(blockwidth * n, blockheight * 7, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(c));
+  fill(color(c));
   rect(blockwidth * n, blockheight * 8, blockwidth, blockheight)
-  fill(color(a));
+  fill(color(c));
+  fill(color(c));
   rect(blockwidth * n, blockheight * 9, blockwidth, blockheight)
 
 var n = 3;
